a2ff71d18ecef0d1a0e12c39bcf91f81d421d248
YutoOzaki <yutoozaki@hotmail.com>
2023-07-06 10:40:32 +0900

    Add existing file

--- test10_chirprate.m
+++ test10_chirprate.m
@@ -30,61 +30,71 @@ function test10_chirprate
 
     addpath('C:\Users\yuto\Documents\MATLAB\lib2\numdiff');
     X = fft(x);
+    n = (0:numel(x) - 1)./numel(x);
     Omg = zeros(numel(s), numel(x));
     tau = zeros(numel(s), numel(x));
     q = zeros(numel(s), numel(x));
     qhat = zeros(numel(s), numel(x));
+    fhat = zeros(numel(x), 1);
+    chat = zeros(numel(x), 2);
+    gdhat = zeros(numel(x), 1);
     E = zeros(numel(s), 1);
     for i=1:numel(s)
         [H, xiH, xisqH] = morsewavelet(gam, be, k, s(i).*f);
         E(i) = trapz(s(i).*f(1:N/2), H(1:N/2).^2);
         
-        if abs(E(i) - 1) < 1e-8 
-            dH = numdiff.dif(H, 1);
-            xidH = numdiff.dif(xiH, 1);
-    
-            W_H = ifft(X.*H).*sqrt(s(i));
-            W_xiH = ifft(X.*xiH).*sqrt(s(i));
-            W_xisqH = ifft(X.*xisqH).*sqrt(s(i));
-            W_dH = ifft(X.*dH).*sqrt(s(i));
-            W_xidH = ifft(X.*xidH).*sqrt(s(i));
-    
-            Omg(i, :) = (1/s(i)).*W_xiH./W_H;
-            tau(i, :) = t + s(i)/(1i*2*pi).*(W_dH./W_H);
-            q(i, :) = (1i*2*pi)/s(i)^2 .* (W_H.*W_xisqH - W_xiH.^2) ./ (W_H.^2 + W_H.*W_xidH + W_dH.*W_xiH);
-        end
+        dH = numdiff.dif(H, s(i));
+        xidH = numdiff.dif(xiH, s(i));
+
+        W_H = ifft(X.*H).*sqrt(s(i));
+        W_xiH = ifft(X.*xiH).*sqrt(s(i));
+        W_xisqH = ifft(X.*xisqH).*sqrt(s(i));
+        W_dH = ifft(X.*dH).*sqrt(s(i));
+        W_xidH = ifft(X.*xidH).*sqrt(s(i));
+
+        Omg(i, :) = (1/s(i)).*W_xiH./W_H;
+        tau(i, :) = n + s(i)/(1i*2*pi).*(W_dH./W_H);
+        q(i, :) = (1i*2*pi)/s(i)^2 .* (W_H.*W_xisqH - W_xiH.^2) ./ (W_H.^2 + W_H.*W_xidH + W_dH.*W_xiH);
     end
     q = real(q);
-    
+
     for i=1:numel(x)
         qhat(:, i) = real(numdiff.dif(Omg(:, i), 1)./numdiff.dif(tau(:, i), 1));
+
+        f_i = f0 + c*(i - 1)/fs;
+        s_i = (f_i/fs/omg_c)^-1;
+        [~, idx] = min(abs(s_i - s));
+
+        fhat(i) = Omg(idx, i);
+        gdhat(i) = tau(idx, i);
+        chat(i, 1) = q(idx, i);
+        chat(i, 2) = qhat(idx, i);
     end
-    qhat = real(qhat);
+    fhat = real(fhat);
+    gdhat = real(gdhat);
+    chat = real(chat);
 
     %% validation
     figure(2);
     subplot(2, 1, 1); plot(E);
     subplot(2, 1, 2); plot(abs(E - 1) < 1e-8);
     
-    i = randi(100) + 10;
-    f_s = omg_c/s(i)*fs;
-    t_s = (f_s - f0)/c;
-    [~, idx] = min(abs(t - t_s));
-    
     figure(3);
-    clf; cla;
-    plot(t, fs.*q(i, :));
-    hold on;
-    plot(t, fs.*qhat(i, :));
-    yl = ylim(); plot([t_s, t_s], yl, '-.m');
+    plot(t, fhat.*fs);
+    hold on
+    plot(t, f0 + c.*t, '-.m');
     hold off
-    title(sprintf('f = %3.4f, IF = %3.4f, c = %3.4f, qhat = %3.4f, q = %3.4f',...
-        f0 + c*t_s, fs*real(Omg(i, idx)), c, fs*qhat(i, idx), fs*q(i, idx)));
-
+    
     figure(4);
-    plot(t, real(tau(i, :) - t));
-    hold on;
-    yl = ylim(); plot([t_s, t_s], yl, '-.m');
+    plot(t, gdhat.*(N/fs))
+    hold on
+    plot(t, t, '-.m');
     hold off
-    title(sprintf('dt = %3.4f, dtau = %3.4f', t(idx) - t_s, real(tau(i, idx) - t(idx))));
+
+    figure(5);
+    plot(t, chat(:, 1));
+    hold on
+    plot(t, chat(:, 2))
+    hold off
+    title(sprintf('c = %3.4f', c));
 end
