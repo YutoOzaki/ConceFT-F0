6033ffd1acd0ed815ba639cec996b3719fd80afc
YutoOzaki <yutoozaki@hotmail.com>
2023-07-06 09:38:34 +0900

    Add existing file

--- test10_chirprate.m
+++ test10_chirprate.m
@@ -30,15 +30,18 @@ function test10_chirprate
 
     addpath('C:\Users\yuto\Documents\MATLAB\lib2\numdiff');
     X = fft(x);
+    Omg = zeros(numel(s), numel(x));
+    tau = zeros(numel(s), numel(x));
     q = zeros(numel(s), numel(x));
+    qhat = zeros(numel(s), numel(x));
     E = zeros(numel(s), 1);
     for i=1:numel(s)
         [H, xiH, xisqH] = morsewavelet(gam, be, k, s(i).*f);
         E(i) = trapz(s(i).*f(1:N/2), H(1:N/2).^2);
         
         if abs(E(i) - 1) < 1e-8 
-            dH = numdiff.dif(H, s(i));
-            xidH = numdiff.dif(xiH, s(i));
+            dH = numdiff.dif(H, 1);
+            xidH = numdiff.dif(xiH, 1);
     
             W_H = ifft(X.*H).*sqrt(s(i));
             W_xiH = ifft(X.*xiH).*sqrt(s(i));
@@ -46,69 +49,41 @@ function test10_chirprate
             W_dH = ifft(X.*dH).*sqrt(s(i));
             W_xidH = ifft(X.*xidH).*sqrt(s(i));
     
+            Omg(i, :) = (1/s(i)).*W_xiH./W_H;
+            tau(i, :) = t + s(i)/(1i*2*pi).*(W_dH./W_H);
             q(i, :) = (1i*2*pi)/s(i)^2 .* (W_H.*W_xisqH - W_xiH.^2) ./ (W_H.^2 + W_H.*W_xidH + W_dH.*W_xiH);
         end
     end
     q = real(q);
+    
+    for i=1:numel(x)
+        qhat(:, i) = real(numdiff.dif(Omg(:, i), 1)./numdiff.dif(tau(:, i), 1));
+    end
+    qhat = real(qhat);
 
     %% validation
     figure(2);
     subplot(2, 1, 1); plot(E);
     subplot(2, 1, 2); plot(abs(E - 1) < 1e-8);
     
-    i = 40;
+    i = randi(100) + 10;
     f_s = omg_c/s(i)*fs;
     t_s = (f_s - f0)/c;
     [~, idx] = min(abs(t - t_s));
-
-    [H, xiH, ~] = morsewavelet(gam, be, k, s(i).*f);
-    dH = numdiff.dif(H, s(i));
-    W_H = ifft(X.*H).*sqrt(s(i));
-    W_xiH = ifft(X.*xiH).*sqrt(s(i));
-    W_dH = ifft(X.*dH).*sqrt(s(i));
-
-    Omg = (1/s(i)).*W_xiH./W_H;
-    tau = t + s(i)/(1i*2*pi).*(W_dH./W_H);
-    qhat = real(numdiff.dif(Omg, 1)./numdiff.dif(tau, 1));
     
     figure(3);
     clf; cla;
     plot(t, q(i, :));
     hold on;
-    plot(t, qhat);
+    plot(t, qhat(i, :));
     yl = ylim(); plot([t_s, t_s], yl, '-.m');
     hold off
-
-    figure(2);
-    clf; cla;
-    plot(t, real(tau));
-    hold on;
-    yl = ylim(); plot([t_s, t_s], yl, '-.m');
-    xl = xlim(); plot(xl, real([tau(idx), tau(idx)]), '-.b');
-    hold off;
-    
-    figure(3);
-    subplot(2, 1, 1);
-    plot(f, H);
-    E = trapz(f(1:N/2).*s, H(1:N/2).^2);
-    title(sprintf('E = %e', E));
-    subplot(2, 1, 2);
-    plot(f, dH);
+    title(sprintf('f = %3.4f, IF = %3.4f, c = %3.4f', f0 + c*t_s, fs*real(Omg(i, idx)), c/fs));
 
     figure(4);
-    clf; cla;
-    plot(t, real(dtau));
+    plot(t, real(tau(i, :) - t));
     hold on;
     yl = ylim(); plot([t_s, t_s], yl, '-.m');
-    xl = xlim(); plot(xl, real([dtau(idx), dtau(idx)]), '-.b');
-    hold off;
-    
-    figure(5);
-    subplot(2, 1, 1);
-    plot(t, abs(W_H).^2);
-    hold on; yl = ylim(); plot([t_s, t_s], yl, '-.m'); hold off
-    title(sprintf('f_W = %3.4f [Hz]', f_s));
-    subplot(2, 1, 2);
-    plot(t, abs(W_dH).^2);
-    hold on; yl = ylim(); plot([t_s, t_s], yl, '-.m'); hold off
+    hold off
+    title(sprintf('dt = %3.4f, dtau = %3.4f', t(idx) - t_s, real(tau(i, idx) - t(idx))));
 end
